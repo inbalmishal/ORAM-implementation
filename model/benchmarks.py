95d75ec03972d3139ca89f3088a17e7f0e2888e3
inbalmishal <inbal.mishal3@gmail.com>
2022-05-08 04:43:32 +0300

    add benchmarks file

--- model/benchmarks.py
+++ model/benchmarks.py
@@ -12,9 +12,9 @@ def benchmarks(client, req_num):
     """
     # calculate throughput
     start = time.time()
-    for i in range(req_num // 2):
+    for i in range(req_num // 3):
         client.upload_file(FILE_PATH, 'file')
-        # file, real_size = client.get_file('file')
+        file, real_size = client.get_file('file')
         client.delete_file('file')
     end = time.time()
     throughput = req_num / (end - start)
@@ -32,22 +32,21 @@ if __name__ == '__main__':
     with open('benchmarks2.txt', 'w') as f:
         pass
 
-    req_num_arr = [12, 21, 33, 54, 106, 207]
-    N_arr = [2, 4, 8]  # N is the number of leaves (files)
+    req_num_arr = [15, 30, 60, 150, 210]
+    N = 128  # 2, 4, 8, 16, 32, 64  # N is the number of leaves (files)
     throughput_arr = []
     latency_arr = []
 
-    for N in N_arr:
-        client = Client(N)
-        for r in req_num_arr:
-            throughput, latency = benchmarks(client, r)
-            throughput_arr.append(throughput)
-            latency_arr.append(latency)
-            # download the current data
-            with open('benchmarks2.txt', 'a') as f:
-                f.write(f'throughput = {throughput}, latency = {latency}, N = {N}, req_num = {r}\n')
-            print("--------------")
-        client.close_connection()
+    client = Client(N)
+    for r in req_num_arr:
+        throughput, latency = benchmarks(client, r)
+        throughput_arr.append(throughput)
+        latency_arr.append(latency)
+        # download the current data
+        with open('benchmarks.txt', 'a') as f:
+            f.write(f'throughput = {throughput}, latency = {latency}, N = {N}, req_num = {r}\n')
+        print("--------------------------------------------------------------------------------------")
+    client.close_connection()
 
 
 
